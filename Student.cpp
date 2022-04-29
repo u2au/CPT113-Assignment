c3d89b7a4b011230dc46e428d58f8adc99b1e3c0
Rinsuki <25454024+rinsukiyo@users.noreply.github.com>
2022-04-30 00:12:33 +0800

    Commit #2

--- Student.cpp
+++ Student.cpp
@@ -3,7 +3,9 @@
 //
 
 #include "Student.h"
+#include "Fee.h"
 #include <iostream>
+#include <fstream> // ifstream
 
 using namespace std;
 
@@ -12,7 +14,32 @@ void welc() {
     cout << "Welcome to use Fee Calculator! \n";
 }
 
+// Read from file
+void readFile(double num, double hostel, bool isInt, int insurance, bool vac, int meal, bool freshman){
+    ifstream file;
+    file.open("student.txt");
+    if (file.is_open()){
+        file >> num; // Matric Number
+        file >> hostel;
+        file >> isInt;
+        file >> insurance;
+        file >> vac;
+        file >> meal;
+        file >> freshman;
+    }
+    else {
+        cout << "Unable to open file\n";
+    }
+
+}
+
+
 // Default Constructor
 Student::Student() {
     welc();
+    readFile(&matricNum, &livesInHostel, &isInternational, &insuranceLevel,
+             &isFullyVaccinated, &monthlyMealPlan, &isFreshman);
+    Fee();
+
+
 }
