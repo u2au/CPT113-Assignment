01beb3afb73f67b521c82b54863439833812f447
emovodka <92780051+emovodka@users.noreply.github.com>
2022-05-06 00:22:42 +0800

    拆分fee说明

--- Fee.h
+++ Fee.h
@@ -6,7 +6,22 @@
 #define CPT113_ASSIGNMENT_FEE_H
 
 #include "Student.h"
+/*fee 分开，拆成:5个class，
+    1：hostelFee， 
+    2：学费和保险，
+    3：international，学费和保险当composition（国际生翻几倍），然后额外加visaFee和隔离费
+    4. MealAndTransportationFee，上面几个随便拉一个当friend function
 
+    5.totalFee，是student class 的dervied class。
+        *用6来算总共fee。
+    totalFee里用student class里的一些数据（year num，sem num），
+    得到totalSemNum: (y1s1=1,y1s2=2,y2s1=2+1,y2s2=2X2,y3s1=2x2+1,y3s2=2X3,y4s1=2x3+1,y4s2=2X4)
+   
+    student class做object array：
+        student hana【totalSemNum】；
+        用operator + overloaded function来计算总费用
+
+*/
 class Fee : public Student {
 private:
     double hostelFee,
@@ -26,6 +41,7 @@ public:
 
     bool getVaccinationStatus();
     bool checkIntl();
+
 
 
 };
