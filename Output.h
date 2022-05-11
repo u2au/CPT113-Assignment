4c2944c69dfa01bbf6a9d13b323b6f3a64bf9c3e
emovodka <92780051+emovodka@users.noreply.github.com>
2022-05-11 15:52:58 +0800

    friend

--- Output.h
+++ Output.h
@@ -4,7 +4,7 @@
 
 #ifndef CPT113_ASSIGNMENT_OUTPUT_H
 #define CPT113_ASSIGNMENT_OUTPUT_H
-class Output;
+
 
 #include "Tuition.h"
 
@@ -18,9 +18,7 @@ public:
     Output() {
         
     }
-    double getNeoCost() const {
-        return necCost;
-    }
+  
     // Prototype
   friend void Tuition::calcNecCost(Output&); // a friend function of class Output
 
