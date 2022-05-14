0b5a74d79a669825d76fec0172d0f0f054afbe6b
Rinsuki <25454024+rinsukiyo@users.noreply.github.com>
2022-05-15 03:57:57 +0800

    Experimental Update

--- main.cpp
+++ main.cpp
@@ -7,8 +7,6 @@
 #include <fstream>
 #include <string>
 
-using namespace std;
-
 // Prototype
 void welc();
 void getFileLines(short &);
@@ -112,11 +110,11 @@ int main()
                 // Output END
 
             } else {
-                cout << "Completed! Thanks for using.\n";
+                std::cout << "Completed! Thanks for using.\n";
                 break;
             }
         }
-    } else cout << "Please check if it exists, "
+    } else std::cout << "Please check if it exists, "
                 << "and if the program has permission to access it." << endl;
 
 
@@ -126,11 +124,11 @@ int main()
 
 // Functions
 void welc() {
-    cout << "Welcome to use the Fee Calculator designed by Group 2 for Assignment 1 of CPT113!\n"
-         << "This program may help you convert the choices of students to respective required amounts.\n"
-         << "For example, it can convert the initial data '1' for desa to '600', "
-         << "which means renting for a semester (5 months estimated), 150 per month.\n"
-         << "Kindly press ENTER to start.";
+    std::cout << "Welcome to use the Fee Calculator designed by Group 2 for Assignment 1 of CPT113!\n"
+              << "This program may help you convert the choices of students to respective required amounts.\n"
+              << "For example, it can convert the initial data '1' for desa to '600', "
+              << "which means renting for a semester (5 months estimated), 150 per month.\n"
+              << "Kindly press ENTER to start.";
 
     cin.get();
 }
@@ -147,7 +145,7 @@ void getFileLines(short &line) {
             getline(file, temp);
             line++;
         }
-    } else cout << "Unable to open the file: Student.txt" << endl;
+    } else std::cout << "Unable to open the file: Student.txt" << endl;
 
 
     file.close();
@@ -159,49 +157,49 @@ void inputValidation(int &matric, string &maj, bool &freshman,
 
     // Matric Num (int, 0-999999)
     while (matric < 0 || matric > 999999) {
-        cout << "Invalid matriculation number. Please re-enter (0-999999): ";
+        std::cout << "Invalid matriculation number. Please re-enter (0-999999): ";
         cin >> matric;  // Re-enter the matriculation number
     }
 
     // NEW! Major (string)
     while (maj == "default") {
-        cout << "Invalid major. Please enter a correct major (For example, CS): ";
+        std::cout << "Invalid major. Please enter a correct major (For example, CS): ";
         cin >> maj;
     }
 
     // Freshman? (bool, 0/1)
     while (freshman != 0 && freshman != 1) {
-        cout << "Is " << matric << " a freshman? Please re-enter (0 - No, 1 - Yes): ";
+        std::cout << "Is " << matric << " a freshman? Please re-enter (0 - No, 1 - Yes): ";
         cin >> freshman;  // Re-enter whether a freshman
     }
 
     // Hostel Type (int, 0, 1-4)
     while (desa < 0 || desa > 4) {
-        cout << "Invalid desa type for " << matric << ". Please re-enter (0-4): ";
+        std::cout << "Invalid desa type for " << matric << ". Please re-enter (0-4): ";
         cin >> desa;  // Re-enter the hostel type
     }
 
     // Insurance Level (int, 0, 1-5)
     while (insurance < 0 || insurance > 5) {
-        cout << "Invalid insurance level for " << matric << " . Please re-enter (0-5): ";
+        std::cout << "Invalid insurance level for " << matric << " . Please re-enter (0-5): ";
         cin >> insurance;  // Re-enter the insurance level
     }
 
     // Parking Times (int, 0, 1-100)
     while (parking < 0 || parking > 100) {
-        cout << "Invalid parking times for " << matric << " . Please re-enter (0-100): ";
+        std::cout << "Invalid parking times for " << matric << " . Please re-enter (0-100): ";
         cin >> parking;
     }
 
     // International student? (bool, 0/1)
     while (intl != 0 && intl != 1) {
-        cout << "Is " << matric << " an international student? Please re-enter (0 - No, 1 - Yes): ";
+        std::cout << "Is " << matric << " an international student? Please re-enter (0 - No, 1 - Yes): ";
         cin >> intl;  // Re-enter whether international student
     }
 
     // Fully Vaccinated? (bool, 0/1)
     while (vac != 0 && vac != 1) {
-        cout << "Invalid vaccination status. Please re-enter (0 - Not Completed, 1 - Full): ";
+        std::cout << "Invalid vaccination status. Please re-enter (0 - Not Completed, 1 - Full): ";
         cin >> vac;  // Re-enter whether fully vaccinated
     }
 }
