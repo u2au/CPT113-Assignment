65c4a8c1992292bd50900c959646ccee9e624381
DebrisKaiser <81285017+DebrisKaiser@users.noreply.github.com>
2022-06-14 22:10:33 +0800

    Added, cool interface

--- main.cpp
+++ main.cpp
@@ -6,8 +6,7 @@
 #include <string>
 #include "CourseReg.h"
 #include "CGPA.h"
-#include "CourseReg.cpp"
-#include "CGPA.cpp"
+
 
 using namespace std;
 
@@ -20,10 +19,15 @@ int main()
 
     // Welcome
     string name, matric;
-    cout << "Welcome to use USM CS Student's Registration System! \n"
-         << "Please type your name: "; // Name
+    cout << "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" << endl;
+    cout << "■                                                        ■ \n";
+    cout << "■  Welcome to use USM CS Student's Registration System!  ■ \n" ;
+    cout << "■                                                        ■ \n";
+    cout << "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n" << endl;
+    cout << "-------------------------------------------------------------------------------" << endl;
+    cout<< "＞ Please type your name: "; // Name
     cin >> name;
-    cout << "Please enter your matric number: "; // Matric Num
+    cout << "＞ Please enter your matric number: "; // Matric Num
     cin >> matric;
     cout << "Welcome, " << name << " (" << matric << ")!" << endl; // Greetings
 
@@ -38,18 +42,20 @@ int main()
 void menu(short input)
 {
     // Menu
-    cout << "Main Menu \n"
-         << "1 - Course Registration \n"
-         << "2 - CGPA Calculator \n"
-         << "0 - Exit \n"
-         << "Kindly type num of the function that you want to use: ";
+    cout << "\n■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n" 
+    << "■\t\t\tMain Menu\t\t\t■ \n"
+        << "■\t\t  1 - Course Registration  \t\t■ \n"
+        << "■\t\t    2 - CGPA Calculator    \t\t■ \n"
+        << "■\t\t\t0 - Exit\t\t\t■ \n"
+        << "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n"
+        << "Kindly type num of the function that you want to use: ";
     cin >> input;
 
     // Input Validation
     while (input != 0 && input != 1 && input != 2)
     {
         cout << "Sorry, '" << input << "' is not a valid num for the program. \n"
-             << "Please type num of the function again: ";
+            << "Please type num of the function again: ";
         cin >> input;
     }
 
